15a58c09c5a09dd43ffffe0dac135b75c96d09ce
Dip5849 <dipmondalpc58@gmail.com>
2025-11-10 09:13:33 +0600

    m

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="document_portal",
+    version='1.0.0',
+    author='Dip Mondal',
+    description='A portal for chationg and coparing documents'
+)
+
+packages=find_packages(),
